0758fcaf649ea1902e85525921b3b1f0f872c779
asafahmaddev <shenwatson480@gmail.com>
2024-08-19 10:23:08 +0500

    switch case

---
+++ practiceNewFolder/harry1/switchcase.js
@@ -0,0 +1,16 @@
+var month = 10
+switch (month) {
+    case (1): console.log('junary')
+        break
+    case (2): console.log('february')
+        break
+    case (3): console.log('march')
+        break
+    case (4): console.log('april')
+        break
+    case (5): console.log('may')
+        break
+    case (6): console.log('june')
+        break
+    default: console.log('please valid months numbers')
+}
