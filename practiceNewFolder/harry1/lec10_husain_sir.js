006c79d9befebeaca3472286ebd4f25d4297383a
asafahmaddev <shenwatson480@gmail.com>
2024-08-21 13:00:03 +0500

    Assignment Operator

---
+++ practiceNewFolder/harry1/lec10_husain_sir.js
@@ -0,0 +1,13 @@
+//assignment Operators, 
+// 1- simple, 2-compound
+
+//compound operator
+
+var a = 8;
+var b = 5
+//a = a + b; //real from
+//a += b; //compound operator
+//a -= b; //compound operator
+//a *= b; //compound operator
+a /= b;   //compound operator
+console.log("result", a)
