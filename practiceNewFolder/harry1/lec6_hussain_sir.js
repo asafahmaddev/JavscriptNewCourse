6831a993b471459d368ec1fe755893cb6ae2f802
asafahmaddev <shenwatson480@gmail.com>
2024-08-21 10:34:39 +0500

    variable , let , const

---
+++ practiceNewFolder/harry1/lec6_hussain_sir.js
@@ -0,0 +1,18 @@
+/*variable
+var myname = "asaf ahmad";
+var myname = "computer science";
+console.log(myname);\
+*/
+/*
+let
+
+let myname = "Asaf Ahmad";
+myname = "data science";
+console.log(myname);
+*/
+/*
+const
+const myname = "jawad"
+
+console.log(myname);
+*/
