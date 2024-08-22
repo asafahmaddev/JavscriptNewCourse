5dee1ce55e34f078217ac87b55170d48770fa218
asafahmaddev <shenwatson480@gmail.com>
2024-08-22 12:54:11 +0500

    if else statement

---
+++ practiceNewFolder/harry1/lec15_husainsir.js
@@ -0,0 +1,33 @@
+// if else statement
+/*
+var x = 5;
+var y = 5;
+if (x == y) {
+    console.log("these are equal")
+};
+*/
+
+// if else statement
+//show not equal
+/*
+var x = 5;
+var y = 20;
+if (x == y) {
+    console.log("Good these are equal")
+}
+else {
+    console.log("not equaL")
+
+};
+*/
+// if else statement
+//show in equal
+var x = 20;
+var y = 20;
+if (x == y) {
+    console.log("Good these are equal")
+}
+else {
+    console.log("not equaL")
+
+};
