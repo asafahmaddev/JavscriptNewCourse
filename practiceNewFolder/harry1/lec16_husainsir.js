bc496552c77043a43f04133447bf6f0b068c43af
asafahmaddev <shenwatson480@gmail.com>
2024-08-22 13:22:14 +0500

    if else if statement

---
+++ practiceNewFolder/harry1/lec16_husainsir.js
@@ -0,0 +1,19 @@
+// if else if statement
+
+//var time = 2;
+//var time = 14;
+//var time = 17;
+var time = 20;
+
+if (time > 1 && time < 11) {
+    console.log("Good Morning");
+}
+else if (time > 11 && time < 15) {
+    console.log("Good After Noon");
+}
+else if (time > 15 && time < 18) {
+    console.log("Good Evening");
+}
+else {
+    console.log("Good Night")
+};
