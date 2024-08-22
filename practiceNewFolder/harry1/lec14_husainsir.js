0ac0fce6a148c596ff2908d9170da554accd504e
asafahmaddev <shenwatson480@gmail.com>
2024-08-22 12:36:40 +0500

    logical operators // && and , || OR , ! NOT

---
+++ practiceNewFolder/harry1/lec14_husainsir.js
@@ -0,0 +1,68 @@
+//logical operators
+// && and , || OR , ! NOT
+
+
+/*
+var age = 40;
+if (age >= 18) {
+    console.log("this is a true")
+};
+*/
+
+//&& AND operators
+
+var age = 35;
+if (age >= 18 && age <= 40) {
+    console.log("this is a true")
+};
+
+
+//&& AND operators
+/*
+var x = 10;
+var y = 10;
+if (x == 10 && y == 10) {
+    console.log("true condation");
+};
+*/
+
+//|| OR Operator
+/*
+var x = 10;
+var y = 10;
+if (x == 10 || y == 10) {
+    console.log("true my condation");
+};
+*/
+
+//|| OR Operator
+/*
+var x = 6;
+var y = 10;
+if (6 == 10 || y == 10) {
+    console.log("true my condation..");
+};
+*/
+
+// ! NOT Operators
+/*
+var x = 5;
+var y = 10;
+console.log(!x == y);
+*/
+
+//! NOT Operators
+/*
+var x = 10;
+var y = 10;
+console.log(!x == y);
+*/
+
+//! NOT Operators
+/*
+var x = 10;
+var y = 10;
+console.log(x == y);
+*/
+
+
