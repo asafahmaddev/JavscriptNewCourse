8851ef7878a9ee56c2e162f62218469f46ddf4cb
asafahmaddev <shenwatson480@gmail.com>
2024-08-22 11:50:31 +0500

    if statement

---
+++ practiceNewFolder/harry1/lec13_husain_sir.js
@@ -0,0 +1,26 @@
+// if statement
+
+var x = 5;
+
+var y = 5;
+
+//var y = "5";
+
+//var x = 20;
+//var y = 5;
+
+
+if (x == y) {
+    console.log("this is true cond");
+};
+
+/*
+if (x >= y) {
+    console.log("this is true");
+};
+*/
+/*
+if (x <= y) {
+    console.log("this is true co");
+};
+*/
