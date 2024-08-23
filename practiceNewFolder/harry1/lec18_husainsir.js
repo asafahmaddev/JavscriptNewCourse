045faba3a03959c6774696fb1e19a81cc808d4f9
asafahmaddev <shenwatson480@gmail.com>
2024-08-23 11:02:57 +0500

    switch case

---
+++ practiceNewFolder/harry1/lec18_husainsir.js
@@ -0,0 +1,42 @@
+//switch case
+
+var month = 12
+switch (month) {
+    case (1): console.log('january')
+        break
+    case (2): console.log('february')
+        break
+    case (3): console.log('march')
+        break
+    case (4): console.log('april')
+        break
+    case (5): console.log('may')
+        break
+    case (6): console.log('june')
+        break
+    case (7): console.log('Julay')
+        break
+    case (8): console.log('August')
+        break
+    case (9): console.log('September')
+        break
+    case (10): console.log('October')
+        break
+    case (11): console.log('Number')
+        break
+    case (12): console.log('December')
+        break
+    default: console.log('please valid months numbers')
+}
+
+//comparison operator
+/*
+var age = 15;
+//var age = 25;
+//var age = 67;
+switch (true) {
+ case (age >= 18 && age <= 40): console.log('you are aligible')
+     break
+ default: console.log('you are not aligible')
+}
+ */
