a7ca6ef5b00614e5d2cd215e245534e6ceaec244
asafahmaddev <shenwatson480@gmail.com>
2024-08-23 10:37:56 +0500

    conditional Ternary Operator variablename = (condition) ? /value1 = value2

---
+++ practiceNewFolder/harry1/lec17_husainsir.js
@@ -0,0 +1,23 @@
+// conditional Ternary Operator
+//variablename = (condition) ?
+// value1 = value2
+/*
+//var age = 18;
+var age = 14;
+var drive;
+if (age >= 17) {
+    drive = 'yes drive';
+
+}
+else {
+    drive = 'NO Drive'
+};
+console.log("result", drive); 
+*/
+
+
+// conditional Ternary Operator
+var age = 38;
+//var age = 20;
+var drive = age >= 30 ? 'yes drive' : 'No drive'
+console.log("result is", drive);
