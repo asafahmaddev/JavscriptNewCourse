2f57a145315cdf123d1ffd7d63a19be7282cbe51
asafahmaddev <shenwatson480@gmail.com>
2024-08-23 12:36:33 +0500

    prompt box in javascript

---
+++ practiceNewFolder/harry1/lec21_husainsir.js
@@ -0,0 +1,40 @@
+//prompt box in javascript
+
+//prompt('Enter Your Name ?');
+/*
+var a = prompt('Enter Your Name ?');
+alert(a);
+*/
+/*
+var a = prompt('Enter Your Name ?');
+//console.log(a);
+console.log('welcome to' + a);
+*/
+
+var a = prompt('Enter number of days');
+switch (true) {
+
+    case (a == 1): console.log('Sunday')
+        break;
+
+    case (a == 2): console.log('Monday')
+        break;
+
+    case (a == 3): console.log('Tuesday')
+        break;
+
+    case (a == 4): console.log('wednesday')
+        break;
+
+    case (a == 5): console.log('Truesday')
+        break;
+
+    case (a == 6): console.log('Friday')
+        break;
+
+    case (a == 7): console.log('Saturday')
+        break;
+
+    default: console.log('Eenter valid day number')
+
+};
