418938fc35c446d88a16366e9f688241ba5ce60c
asafahmaddev <shenwatson480@gmail.com>
2024-08-23 11:44:32 +0500

    confirm box in alert, output problem

---
+++ practiceNewFolder/harry1/lec20_husainsir.js
@@ -0,0 +1,16 @@
+//confirm box in alert
+
+//confirm('Do you like this website ?');
+/*
+var a = confirm('Do you like this website ?');
+alert(a);
+*/
+
+var a = confirm('Do you like this website ?');
+if (a) {
+    alert('thanks to like this website.');
+}
+else {
+    alert('It is ok');
+};
+
