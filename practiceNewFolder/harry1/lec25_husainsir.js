8764c0154e90c42e65cfd14ee20871af15669dfd
asafahmaddev <shenwatson480@gmail.com>
2024-08-24 12:37:45 +0500

    Global varibales and Local variables

---
+++ practiceNewFolder/harry1/lec25_husainsir.js
@@ -0,0 +1,13 @@
+// Global varibales and Local variables
+//global varibale  ba da function na bahar jory.de ta way global variable.
+//local variable ba pa function danana  use ke aw de ta way local variable.
+//local var  pa global var ke nashe use kawre, aw global variable pa local variable ke use kaware she,
+//global var har zai ke use kaware she, global var pa function ke danana aw bahar dono main use krskte hai.
+
+var a = 'idrees khan';
+function abc() {
+    var b = 'Ashraf';
+    console.log(b);
+    //console.log(a);
+};
+abc();
