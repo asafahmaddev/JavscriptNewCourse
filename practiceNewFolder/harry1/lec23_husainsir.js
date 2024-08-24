259fe0905a299c8ce2eaa26b3aec14dfa03ffef9
asafahmaddev <shenwatson480@gmail.com>
2024-08-24 10:57:17 +0500

    Function with parameters  ,output error

---
+++ practiceNewFolder/harry1/lec23_husainsir.js
@@ -0,0 +1,32 @@
+// Function with parameters
+/*
+function fullname(fnmae, lname) {
+    console.log('Hello' + fnmae + '' + lname);
+
+};
+fullname('ashraf', 'khan');
+fullname('ali', 'Raza');
+fullname('sami', 'khan');
+*/
+
+/*
+function fullname(ali, Raza) {
+    fullname('Hello' + fname = 'asharf' + '' + lname = 'khan');
+    console.log('Hello' + fname + ' ' + lname)
+
+};
+fullname();
+*/
+function sum(a, b) {
+    console.log(a + b + '<br>');
+
+};
+
+sum(25, 30);
+sum(24, 98);
+sum(20, 90);
+sum(40, 80);
+
+
+
+
