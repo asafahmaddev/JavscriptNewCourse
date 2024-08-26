7afeba926008f2999dbd4bedd17ef0d87e150286
asafahmaddev <shenwatson480@gmail.com>
2024-08-26 12:09:03 +0500

    add events listner in javascript

---
+++ practiceNewFolder/harry1/lec30_htmlcs.js
@@ -0,0 +1,35 @@
+//  addeventlistner
+
+/*
+document.getElementById('img1').onmouseover = abc;
+
+function abc() {
+
+    document.getElementById('img1').style.filter = 'grayscale(0%)';
+}
+
+
+document.getElementById('img1').onmouseout = xyz;
+
+function xyz() {
+
+    document.getElementById('img1').style.filter = 'grayscale(100%)';
+}
+    */
+
+document.getElementById('img1').addEventListener("mouseover", function () {
+    this.style.filter = 'grayscale(0%)';
+});
+
+document.getElementById('img1').addEventListener("mouseout", function () {
+    this.style.filter = 'grayscale(100%)';
+});
+
+
+//img2
+document.getElementById('img2').addEventListener("mouseover", function () {
+    this.style.filter = 'grayscale(0%)';
+});
+document.getElementById('img2').addEventListener("mouseout", function () {
+    this.style.filter = 'grayscale(100%)';
+});
