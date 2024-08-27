ecc3ee434208e57ccb3fc6848ea95f1fa688750d
asafahmaddev <shenwatson480@gmail.com>
2024-08-27 12:08:45 +0500

    Array, in different mehods

---
+++ practiceNewFolder/harry1/lec34_array_husainsir.js
@@ -0,0 +1,31 @@
+// Array
+/*
+let sub1 = 'js';
+let sub2 = 'java'
+let sub3 = 'vp';
+let sub4 = 'ap';
+//console.log('result', sub3,);
+console.log('result', sub2, sub3, sub1, sub4);
+*/
+
+//2nd mehtod
+/*
+let subject = new Array('js', 'vp', 'ap', 'java', 'coa');
+console.log('result', subject);
+*/
+
+// 3rd method
+/*
+let subject = ['js', 'vp', 'ap', 'java', 'coa'];
+console.log('result:', subject);
+*/
+/*
+let subject = ['js', 'vp', 'ap', 'java', 'coa'];
+console.log('result:', subject[2], subject[4]);
+*/
+
+let subject = ['js', 'vp', 'ap', 'java', 'coa'];
+
+subject[3] = 'paytan'
+console.log('result:', subject[3]);
+
