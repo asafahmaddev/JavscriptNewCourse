a8edcd6d750095bee595a0e13f6c0a74f013dc96
asafahmaddev <shenwatson480@gmail.com>
2024-08-28 11:14:55 +0500

    Date Method, get and set

---
+++ practiceNewFolder/harry1/lec38_datemethod-husain.js
@@ -0,0 +1,54 @@
+// Date Method
+/*
+var d = new Date();
+console.log(d);
+*/
+
+/*
+// apne marze se date select krta hai yaha
+var d = new Date('may, 11, 1997');
+console.log(d);
+*/
+// use in different properities
+/*
+var d = new Date();
+// console.log(d.toDateString());
+
+// console.log(d.getFullYear());
+
+//console.log(d.getDate());
+
+// console.log(d.getHours());
+//console.log(d.getMinutes());
+
+//console.log(d.getSeconds());
+
+//console.log(d.getMilliseconds());
+
+//console.log(d.getTime());
+
+//console.log(d.getDay());
+*/
+
+//set
+
+var d = new Date();
+// console.log(d.setFullYear(2010));
+
+//console.log(d.setDate(2010));
+
+//console.log(d.setDay());
+
+//console.log(d.setHours());
+
+//console.log(d.setMilliseconds());
+//console.log(d.setMinutes());
+
+//console.log(d.setMonth());
+
+// console.log(d.setSeconds());
+
+console.log(d.setTime());
+
+
+console.log('result', d);
