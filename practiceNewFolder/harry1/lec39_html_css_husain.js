52c93c02da7c0396da7cb1e435a29c9e42ad6ba0
asafahmaddev <shenwatson480@gmail.com>
2024-08-28 13:12:18 +0500

    DOM:Document Object Model in Javascript

---
+++ practiceNewFolder/harry1/lec39_html_css_husain.js
@@ -0,0 +1,16 @@
+//
+/*
+var headerTitle =
+    document.getElementById('header-title');
+
+headerTitle.textcontent = "computer course";
+headerTitle.innerText = "pro course";
+console.log(headerTitle);
+*/
+
+var headerTitle =
+    document.getElementById('header-title');
+headerTitle.innerHTML = <h1>Hello</h1>;
+
+var header = document.getElementById('header');
+header.style.borderBottom = '1px solid #000';
